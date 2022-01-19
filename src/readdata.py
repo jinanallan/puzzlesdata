0e96e285d97621ae4a6fed44d982281b3df2db81
jinanallan <jnallan@ou.edu>
2022-01-19 13:38:34 +0100

    parsing the filenames

--- src/readdata.py
+++ src/readdata.py
@@ -4,15 +4,24 @@ import pandas as pd
 print('Reading data')
 
 path_to_json = 'logs/'
-json_files = [pos_json for pos_json in os.listdir(path_to_json) if pos_json.endswith('.json')]
+json_files = [pos_json for pos_json in sorted(os.listdir(path_to_json)) if pos_json.endswith('.json')]
 print(json_files)
 
 jsons_data = pd.DataFrame(columns=['end-status','end-time', 'start-time', 'solved', 'total-time'])
+cntSolved = 0
+testDict = {}
+testDict['prtcpID'] = 0
+testDict['puzzles'] = {}
 
 # we need both the json and an index number so use enumerate()
 for index, js in enumerate(json_files):
+  print(js)
+  tokens = js.split('_')
+  puzzleID = tokens[1]
+  print(tokens)
   with open(os.path.join(path_to_json, js)) as json_file:
     json_text = json.load(json_file)
+    ###testDict['puzzles'][puzzleID].append(json_text)
 #    print(json_text)
 
     endstatus = json_text['end-status']
@@ -21,8 +30,11 @@ for index, js in enumerate(json_files):
     totaltime = json_text['total-time']
     solved = json_text['solved']
     puzzlefile = json_text['puzzle-file']
+    if solved == True:
+      cntSolved = cntSolved + 1
 
     jsons_data.loc[index] = [endstatus, endtime, starttime, solved, totaltime]
 
 print(jsons_data)
+print('Solved', cntSolved)
 
