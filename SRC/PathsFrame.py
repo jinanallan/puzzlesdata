11a2e8a648986e7bd2234b4d9f60902af7f3c4d5
Erfan <baradarantohidi@gmail.com>
2023-11-16 19:09:38 +0100

    joint plot of attachment and path for puzzle 21 to 26

--- SRC/PathsFrame.py
+++ SRC/PathsFrame.py
@@ -4,14 +4,16 @@ import re
 import json
 import pandas as pd
 import matplotlib.pyplot as plt
-import matplotlib as mpl
 from PIL import Image
 import time
 import movementTracker
 import json
 import subprocess
+
 start_time = time.time()
-def coloring(object):
+
+def coloring(object,dummy = True):
+    if dummy:
         if object=='box1':
             return (0,0,1) 
         elif object=='box2':
@@ -28,8 +30,25 @@ def coloring(object):
             return (0,1,1) 
         elif object=='ego':
             return (0,0,0) 
+    else:
+        if object=='box1':
+            return [(0,0,1,c) for c in np.linspace(0,1,100)]
+        elif object=='box2':
+            return [(0,1,0,c) for c in np.linspace(0,1,100)]
+        elif object=='obj1':
+            return [(1,0,0,c) for c in np.linspace(0,1,100)]
+        elif object=='obj1_a':
+            return [(1,0.5,0,c) for c in np.linspace(0,1,100)]
+        elif object=='obj2':
+            return [(1,0,1,c) for c in np.linspace(0,1,100)]
+        elif object=='obj3':
+            return [(1,1,0,c) for c in np.linspace(0,1,100)]
+        elif object=='obj4':
+            return [(0,1,1,c) for c in np.linspace(0,1,100)]
+        elif object=='ego':
+            return [(0,0,0,c) for c in np.linspace(0,1,100)]
     
-def positional_vector(data):
+def positional_vector(data, ignore_ego=False):
     """
     Get the positional vector of the objects from frames json file
 
@@ -37,7 +56,7 @@ def positional_vector(data):
         data: the json file
     Reurns: 
         positional_vector: dataframe with the positional vector
-        object_names: dict of object names and their IDs
+        present_objects: dict of object names and their IDs
     """
     data = pd.DataFrame(data)
 
@@ -45,15 +64,16 @@ def positional_vector(data):
     present_objects = {}
     for definition in last_frame:
         present_objects[definition["ID"]] = definition["name"]
-    # print(present_objects)
 
     universal_Objects = ["box1","box2", "obj1","obj2", "obj3","obj4","ego","obj1_a"]
+    if ignore_ego:
+        universal_Objects.remove("ego")
     
     for x in list(present_objects):
         if present_objects[x] not in universal_Objects:
             # print(present_objects[x])
             present_objects.pop(x)
-    # print(present_objects)
+    
     positional_vector=pd.DataFrame(columns=present_objects)
     sub_columns = pd.MultiIndex.from_product([positional_vector.columns, ['x', 'y']], names=['ID', 'position'])
     positional_vector = pd.DataFrame(index=range(len(data.frames)), columns=sub_columns)
@@ -83,6 +103,78 @@ def use_regex(input_text):
     puzzle_id = match.group(5)
     attempt = match.group(6)
     return int(particpants), int(run), int(puzzle_id), int(attempt)
+  
+def attachment_plot(positional_vector, present_objects, fig, ax):
+    """
+    Plot the attachment of objects to the during the solution in form of: whether the object is moving or not 
+    and if moving at the same time as another object, then they are attached (valid for pick and place puzzles)
+
+    Accepts:
+        positional_vector: dataframe with the positional vector
+        present_objects: dict of object names and their IDs
+    Reurns:
+        Plot of the attachment of objects to the during the solution
+    """
+    T = len(positional_vector.index)
+    #create a figure to pass as function output
+
+    velocity_vector = positional_vector.diff()
+    velocity_vector = velocity_vector.drop(0)
+    velocity_vector = velocity_vector.reset_index(drop=True)
+    v=np.zeros((len(velocity_vector),len(present_objects)))
+    for i, object_i in enumerate(present_objects):
+        # print (i, object_i)
+        object_i_name = present_objects[object_i]
+        vx_i = velocity_vector[positional_vector.columns[i*2][0],'x']
+        vx_i=np.array(vx_i, dtype=np.float64)
+        vy_i = velocity_vector[positional_vector.columns[i*2][0],'y']
+        vy_i=np.array(vy_i, dtype=np.float64)
+        v_temp=np.sqrt(vx_i**2 + vy_i**2)
+        v[:,i]=v_temp
+
+        attachment = []
+        start_time = None
+        for t in np.arange(0,T-1):
+            if v_temp[t] > 0:
+                #  plt.scatter(t, i, color=coloring(present_objects[object_i], True), s=50, marker='s')
+                start_time = t
+            elif start_time is not None:
+                attachment.append([start_time, t])
+                start_time = None
+
+        if attachment != []:
+
+            start_time = attachment[0][0]
+            end_time = attachment[0][1]
+            modified_attachment = []
+
+            for attach_index in range(1,len(attachment)):
+
+                if attachment[attach_index][0] - end_time < 50:
+                    end_time = attachment[attach_index][1]
+                else:
+                    modified_attachment.append([start_time, end_time])
+                    start_time = attachment[attach_index][0]
+                    end_time = attachment[attach_index][1]
+            modified_attachment.append([start_time, end_time])
+            # print(modified_attachment)
+                    
+
+            for attach in modified_attachment:
+                ax.barh(y=i/2, width=attach[1]-attach[0], left=attach[0], height=0.5, color=coloring(present_objects[object_i], True))
+    
+    # for t in np.arange(0,T-1):
+    #     for i, object_i in enumerate(present_objects):
+    #         for j in range(i+1,len(present_objects)):
+    #             object_j = list(present_objects.keys())[j]
+    #             if v[t,i]>0 and v[t,j]>0:
+    #                 plt.scatter(t, j/2, color="black", s=20, marker='*')
+    #                 plt.scatter(t, i/2, color="black", s=20, marker='*')
+    ax.set_xlabel('Time [s]')
+    # ax.set_ylabel('Object name')
+    ax.set_yticks(np.arange(len(present_objects))/2, present_objects.values())
+    ax.set_xticks(np.arange(0,T, 1000), np.arange(0,T/100, 10))
+    return ax
 
 def main():
  for pilot in [3,4]:
@@ -105,22 +197,21 @@ def main():
             except:
                  df = pd.DataFrame(ego_file, index=[0])
             # print(df)
-            print(f'{filecounter}/{n}')
+            # print(f'{filecounter}/{n}')
             filecounter += 1
             if filename.endswith('.json'):
                 participant_id, run, puzzle, attempt = use_regex(filename)
             #check if the file is already plotted
             
-            
             solved , total_time = movementTracker.interaction(df, participant_id, run, "free", solved=True)
             # print(solved, total_time)
      
-            if ( not os.path.isfile('./Plots_Text/Path_Plots/frameBased/includeEgo/'+ str(participant_id)+'_'+ str(run)+'_'+str(puzzle)+'_'+str(attempt)+'.png')) :
+            if ( not os.path.isfile('./Plots_Text/Path_Plots/frameBased/pathAttachment/'+ str(participant_id)+'_'+ str(run)+'_'+str(puzzle)+'_'+str(attempt)+'.png')) and puzzle in [21,22,23,24,25,26] :
                 print("entered:" , str(participant_id)+'_'+ str(run)+'_'+str(puzzle)+'_'+str(attempt)+'.png')
                 with open(os.path.join(folder,filename)) as json_file:
                             
                         data = json.load(json_file)
-                        vector, objects_names = positional_vector(data)
+                        vector, objects_names = positional_vector(data, ignore_ego=True)
                         total_objects_len = len(objects_names)
 
                         # velocity_vector = vector.diff()
@@ -173,52 +264,55 @@ def main():
                         #     print(equal_velocity_parts.keys())
 
                                 
-                        fig, ax = plt.subplots()
+                        mainfig, (ax1, ax2) = plt.subplots(1,2)
+                        ax1 = attachment_plot(vector, objects_names, mainfig, ax1)
+
                         for i in range(total_objects_len):
 
                             object = list(objects_names)[i]
-                            if True: #objects_names[object] != 'ego' in case ignoring ego
+
+                            xi = vector[object]['x']
+                            yi = vector[object]['y']
+                            xi = np.array(xi)
+                            yi = np.array(yi)
+
+                            imgfolder = './cropped_puzzles_screenshots'
+                            fname = os.path.join(imgfolder, 'puzzle'+str(puzzle)+'.png')
+                            img = Image.open(fname).convert('L')
+
+                            img = ax2.imshow(img, extent=[-2, 2, -2, 2], cmap='gray')
+
+                            if xi.size == 0 or yi.size == 0:
+                                continue
+                            else:
+                                t = objects_names[object]
                                 
-    
-                                xi = vector[object]['x']
-                                yi = vector[object]['y']
-                                xi = np.array(xi)
-                                yi = np.array(yi)
-                                # print(xi.size, yi.size)
-                                # print(xi, yi)
+                                #diffrent colors for each type
+                                # colors=[coloring(type,i) for i in np.linspace(0.2,1,len(xi))]
+                                # cm=mcolors.LinearSegmentedColormap.from_list('mylist', colors, N=len(xi))
+                                c=np.arange(0,len(xi))
+                                #map alpha from first to last point in xi
+                                #increase alpha from first to last point in xi
 
+                                ax2.scatter(xi, yi,alpha=0.1, color= coloring(objects_names[object]), s=10, edgecolors='face',
+                                        marker= ".")
+                                sc = ax2.scatter([],[], color=coloring(objects_names[object]), label=objects_names[object])
 
-                                imgfolder = './cropped_puzzles_screenshots'
-                                fname = os.path.join(imgfolder, 'puzzle'+str(puzzle)+'.png')
-                                img = Image.open(fname).convert('L')
+                                ax2.set_xlim(-2, 2)
+                                ax2.set_ylim(-2, 2)
+                                ax2.set_xlabel('x')
+                                ax2.set_ylabel('y')
+                                #set the legend
+                                ax2.legend(loc='center right', bbox_to_anchor=(1.3, 0.5), ncol=1, fancybox=True)
+                                title= f'Participant: {participant_id}, Run: {run}, Puzzle: {puzzle}, Attempt: {attempt}, \n Solved: {solved}, Total time: {total_time} s'.format(participant_id, run, puzzle, attempt, solved, total_time)
+                                mainfig.suptitle(title, fontsize=14)
+                                #set the height of the both axes same
+                                mainfig.set_figheight(5)
+                                mainfig.set_figwidth(10)
+                                mainfig.tight_layout()
+                                mainfig.subplots_adjust(top=0.85)
 
-                                img = ax.imshow(img, extent=[-2, 2, -2, 2], cmap='gray')
-
-                                if xi.size == 0 or yi.size == 0:
-                                    continue
-                                else:
-                                    t = objects_names[object]
-                                    
-                                    #diffrent colors for each type
-                                    # colors=[coloring(type,i) for i in np.linspace(0.2,1,len(xi))]
-                                    # cm=mcolors.LinearSegmentedColormap.from_list('mylist', colors, N=len(xi))
-                                    c=np.arange(0,len(xi))
-                                    #map alpha from first to last point in xi
-                                    #increase alpha from first to last point in xi
-
-                                    ax.scatter(xi, yi,alpha=0.1, color= coloring(objects_names[object]), s=10, edgecolors='face',
-                                            marker= ".")
-                                    sc = plt.scatter([],[], color=coloring(objects_names[object]), label=objects_names[object])
-                                    
-                                    plt.xlim(-2, 2)
-                                    plt.ylim(-2, 2)
-                                    plt.xlabel('x')
-                                    plt.ylabel('y')
-                                    plt.legend(loc='upper right', bbox_to_anchor=(1.3, 1))
-                                    title= f'Participant: {participant_id}, Run: {run}, Puzzle: {puzzle}, Attempt: {attempt}, \n Solved: {solved}, Total time: {total_time} s'.format(participant_id, run, puzzle, attempt, solved, total_time)
-                                    plt.title(title, fontsize=14)
-                                    # plt.show()
-                plt.savefig('./Plots_Text/Path_Plots/frameBased/includeEgo/'+
+                plt.savefig('./Plots_Text/Path_Plots/frameBased/pathAttachment/'+
                                 str(participant_id)+'_'+ str(run)+'_'+str(puzzle)+'_'+str(attempt)+'.png', dpi=300)
                 plt.close()
                                 
@@ -228,10 +322,10 @@ if __name__ == "__main__":
 
     repo_path = './'
 
-# os.chdir(repo_path)
+os.chdir(repo_path)
 
-# subprocess.run(['git', 'add', '.'])
+subprocess.run(['git', 'add', '.'])
 
-# subprocess.run(['git', 'commit', '-m', "frame based path plots with ego added "])
+subprocess.run(['git', 'commit', '-m', "joint plot of attachment and path for puzzle 21 to 26"])
 
 # subprocess.run(['git', 'push'])
--- SRC/attachment.py
+++ SRC/attachment.py
@@ -15,6 +15,7 @@ def use_regex(input_text):
     puzzle_id = match.group(5)
     attempt = match.group(6)
     return int(particpants), int(run), int(puzzle_id), int(attempt)
+
 def positional_vector(data, ignore_ego=False):
     """
     Get the positional vector of the objects from frames json file
@@ -59,6 +60,7 @@ def positional_vector(data, ignore_ego=False):
                 positional_vector.at[row,(id,'y')]=y
         row+=1
     return positional_vector, present_objects
+
 def coloring(object,dummy = False):
     if dummy:
         if object=='box1':
@@ -94,6 +96,7 @@ def coloring(object,dummy = False):
             return [(0,1,1,c) for c in np.linspace(0,1,100)]
         elif object=='ego':
             return [(0,0,0,c) for c in np.linspace(0,1,100)]
+        
 def attachment_plot(positional_vector, present_objects):
     """
     Plot the attachment of objects to the during the solution in form of: whether the object is moving or not 
@@ -107,7 +110,7 @@ def attachment_plot(positional_vector, present_objects):
     """
     T = len(positional_vector.index)
     fig, ax = plt.subplots(figsize=(10,10))
-    
+
     velocity_vector = positional_vector.diff()
     velocity_vector = velocity_vector.drop(0)
     velocity_vector = velocity_vector.reset_index(drop=True)
@@ -121,25 +124,52 @@ def attachment_plot(positional_vector, present_objects):
         vy_i=np.array(vy_i, dtype=np.float64)
         v_temp=np.sqrt(vx_i**2 + vy_i**2)
         v[:,i]=v_temp
-        for t in np.arange(0,T-1):
-                if v_temp[t]>0:
-                    plt.scatter(t, i, color=coloring(present_objects[object_i], True), s=50, marker='s')
 
-    for t in np.arange(0,T-1):
-        for i, object_i in enumerate(present_objects):
-            for j in range(i+1,len(present_objects)):
-                object_j = list(present_objects.keys())[j]
-                if v[t,i]>0 and v[t,j]>0:
-                    plt.scatter(t, j, color="black", s=20, marker='*')
-                    plt.scatter(t, i, color="black", s=20, marker='*')
-    plt.xlabel('time steps [s]')
-    plt.yticks(np.arange(len(present_objects)), present_objects.values())
-    #each time step is 0.01s
-    plt.xticks(np.arange(0,T, 1000), np.arange(0,T/100, 10))
-    plt.savefig('./Plots_Text/attachment_plot_ex.png', dpi=300)
-    plt.show()
-                  
+        attachment = []
+        start_time = None
+        for t in np.arange(0,T-1):
+            if v_temp[t] > 0:
+                #  plt.scatter(t, i, color=coloring(present_objects[object_i], True), s=50, marker='s')
+                start_time = t
+            elif start_time is not None:
+                attachment.append([start_time, t])
+                start_time = None
+
+        if attachment != []:
+
+            start_time = attachment[0][0]
+            end_time = attachment[0][1]
+            modified_attachment = []
+
+            for attach_index in range(1,len(attachment)):
+
+                if attachment[attach_index][0] - end_time < 50:
+                    end_time = attachment[attach_index][1]
+                else:
+                    modified_attachment.append([start_time, end_time])
+                    start_time = attachment[attach_index][0]
+                    end_time = attachment[attach_index][1]
+            modified_attachment.append([start_time, end_time])
+            print(modified_attachment)
                     
+
+            for attach in modified_attachment:
+                ax.barh(y=i/2, width=attach[1]-attach[0], left=attach[0], height=0.5, color=coloring(present_objects[object_i], True))
+    
+    # for t in np.arange(0,T-1):
+    #     for i, object_i in enumerate(present_objects):
+    #         for j in range(i+1,len(present_objects)):
+    #             object_j = list(present_objects.keys())[j]
+    #             if v[t,i]>0 and v[t,j]>0:
+    #                 plt.scatter(t, j/2, color="black", s=20, marker='*')
+    #                 plt.scatter(t, i/2, color="black", s=20, marker='*')
+    ax.set_xlabel('Time [s]',fontsize=16)
+    ax.set_ylabel('Object name',fontsize=16)
+    ax.set_yticks(np.arange(len(present_objects))/2, present_objects.values(), fontsize=14)
+    ax.set_xticks(np.arange(0,T, 1000), np.arange(0,T/100, 10), fontsize=14)
+    plt.show()   
+    return ax
+
     #     for j in range(i+1,len(present_objects)):
     #         object_j = list(present_objects.keys())[j]
     #         print (j, object_j)
@@ -165,7 +195,7 @@ for frame_folder in frame_folders:
             for file in frame_files:
                 if file.endswith(".json"):
                     participant_id, run, puzzle, attempt = use_regex(file)
-                    if puzzle == 6 and run == 1 and attempt == 0 and participant_id == 41:
+                    if puzzle == 26 and run == 1 and attempt == 0 and participant_id == 59:
                         # print(file)
                         with open(frame_folder+file) as f:
                             data = json.load(f)
